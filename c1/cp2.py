f7267f80cefae15afc56cd7841e6a08cdc364690
miranda1867 <cynthiamiranda401@gmail.com>
2025-09-08 19:08:18 -0400

    primer commit

---
+++ c1/cp2.py
@@ -0,0 +1,20 @@
+import sys
+import math
+def di_hola():
+    print("Di Hola")
+
+    print("1. Muestre en la consola el siguiente string: ”Hello World”.")
+    print("Hello World!")
+
+    print("2. Muestre en la consola el valor m´aximo y el valor m´ ınimo admitidos por el tipo int.")
+    print(f'El valor máximo de int es: {sys.maxsize}')
+    print(f'El valor mínimo de int es: {-sys.maxsize - 1}')
+
+    print("3. Muestre en la consola un valor aproximado de Pi (use Math).")
+    print(f'El valor aproximado de Pi es: {math.pi}')
+
+    print("Recibiendo entradas")
+
+def recibiendo_entradas():
+    print("1. Reciba como entrada una cadena de caracteres (string). Luego muestre en la terminal ese mismo string pero con terminación ”medio limón”.")
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from c1 import cp2
+
+cp2.di_hola()
+cp2.recibiendo_entradas()
